019ce80558275d8bf71e626ae41cfd8b33578e68
Wang Tat Lau (k22023660) <wang.lau@kcl.ac.uk>
2023-09-26 00:28:48 +0100

    Update primes.py

--- primes.py
+++ primes.py
@@ -3,4 +3,15 @@
 
 def primes(number_of_primes):
     list = []
+    i = 2
+    con = True
+    while con:
+        list.append(i)
+        for j in range(2,i):
+            if i % j == 0:
+                list.remove(i)
+                break
+        i += 1
+        if len(list) == number_of_primes:
+            con = False
     return list
